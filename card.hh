7949096e1dd920ec4e96be8c6b2974a6e8c7236e
Ruslan Akbashev <ruslashev@gmail.com>
2020-08-09 23:26:18 +0300

    add constants/mnemonics

--- card.hh
+++ card.hh
@@ -26,3 +26,44 @@ union card
 };
 static_assert(sizeof(card) == 4, "card must be 32 bits");
 
+enum suit
+{
+	clubs    = 0b1000,
+	diamonds = 0b0100,
+	hearts   = 0b0010,
+	spades   = 0b0001,
+};
+
+enum rank
+{
+	deuce = 0,
+	trey  = 1,
+	four  = 2,
+	five  = 3,
+	six   = 4,
+	seven = 5,
+	eight = 6,
+	nine  = 7,
+	ten   = 8,
+	jack  = 9,
+	queen = 10,
+	king  = 11,
+	ace   = 12,
+};
+
+static const int primes[] = {
+	[deuce] = 2,
+	[trey]  = 3,
+	[four]  = 5,
+	[five]  = 7,
+	[six]   = 11,
+	[seven] = 13,
+	[eight] = 17,
+	[nine]  = 19,
+	[ten]   = 23,
+	[jack]  = 29,
+	[queen] = 31,
+	[king]  = 37,
+	[ace]   = 41,
+};
+
--- tests.cc
+++ tests.cc
@@ -8,15 +8,15 @@ void tests_card_platform_test()
 	card Kd = { .data = 0b000'0100000000000'0100'1011'00'100101 };
 	card Fs = { .data = 0b000'0000000001000'0001'0011'00'000111 };
 
-	assert(Kd.rank_bit == 1 << 11);
-	assert(Kd.suit     == 0b0100);
-	assert(Kd.rank     == 11);
-	assert(Kd.prime    == 37);
+	assert(Kd.rank_bit == 1 << king);
+	assert(Kd.suit     == diamonds);
+	assert(Kd.rank     == king);
+	assert(Kd.prime    == primes[king]);
 
-	assert(Fs.rank_bit == 1 << 3);
-	assert(Fs.suit     == 0b0001);
-	assert(Fs.rank     == 3);
-	assert(Fs.prime    == 7);
+	assert(Fs.rank_bit == 1 << five);
+	assert(Fs.suit     == spades);
+	assert(Fs.rank     == five);
+	assert(Fs.prime    == primes[five]);
 
 	std::cout << "card platform test: ok" << std::endl;
 }
