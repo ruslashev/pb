997165053f270554fb54811d196cce5b96732b95
Ruslan Akbashev <ruslashev@gmail.com>
2020-08-11 00:54:40 +0300

    add hand evaluation test

--- tests.cc
+++ tests.cc
@@ -24,11 +24,40 @@ void tests_card_platform_test()
 
 void tests_hand_evaluation()
 {
-	card deck[52];
+	card deck[52], hand[5];
+	int freq[10];
 
 	init_deck(deck);
 
-	for (int i = 0; i < 52; ++i)
-		std::cout << deck[i].show() << std::endl;
+	for (int i = 0; i < 10; i++)
+		freq[i] = 0;
+
+	/* loop over every possible five-card hand */
+	for (int a = 0; a < 48; ++a) {
+		hand[0] = deck[a];
+		for (int b = a + 1; b < 49; ++b) {
+			hand[1] = deck[b];
+			for (int c = b + 1; c < 50; ++c) {
+				hand[2] = deck[c];
+				for (int d = c + 1; d < 51; ++d) {
+					hand[3] = deck[d];
+					for (int e = d + 1; e < 52; ++e) {
+						hand[4] = deck[e];
+
+						value v = evalute_hand(hand);
+						hand_rank r = get_hand_rank(v);
+						++freq[r];
+					}
+				}
+			}
+		}
+	}
+
+	uint32_t sum = 0;
+	for (int i = royal_flush; i <= high_card; ++i) {
+		printf("%15s: %8d\n", hand_rank_str[i], freq[i]);
+		sum += freq[i];
+	}
+	printf("%15s: %8d\n", "Total", sum);
 }
 
